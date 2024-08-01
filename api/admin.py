f9e5335613f7370d609cbcf761afc168e7b43f40
rajath-tk <rajath.kurian@gmail.com>
2024-08-01 23:30:33 +0530

    feat: Register additional models in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Student, Teacher, NonTeachingStaff, Subject, Enrollment, Attendance, Exam, ExamResult, Room, Timetable, Event
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Student)
+admin.site.register(Teacher)
+admin.site.register(NonTeachingStaff)
+admin.site.register(Subject)
+admin.site.register(Enrollment)
+admin.site.register(Attendance)
+admin.site.register(Exam)
+admin.site.register(ExamResult)
+admin.site.register(Room)
+admin.site.register(Timetable)
+admin.site.register(Event)
